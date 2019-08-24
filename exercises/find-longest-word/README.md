03279b03670c4eb31f9a3b0af2a64f546cfb797a
kostia bazrow <osetia-alania@mail.ru>
2019-08-24 20:38:36 +0300

    fix first task

---
+++ exercises/find-longest-word/README.md
@@ -0,0 +1,2 @@
+# javascript-crash-course
+Для быстрого просмотра результатов, юзал расширение Node Exec, потом в разделе OUTPUT смарел Node.js
--- exercises/find-longest-word/index.js
+++ exercises/find-longest-word/index.js
@@ -1,13 +1,9 @@
+
 function findLongestWord(str) {
     const words = str.split(" ");
     let longest = ""; 
-    
     words.forEach (word => word.length > longest.length && (longest = word))
-      
-    
-    return `
-        Word: ${longest}
-        Length: ${longest.length}`
+    return `Word: ${longest} \tLength: ${longest.length}`
     }
-    
-    findLongestWord ("Решение о том, какой вариант .");
+    const longestWord = findLongestWord ("Решение о том, какой вариант .");
+    console.log(longestWord)
